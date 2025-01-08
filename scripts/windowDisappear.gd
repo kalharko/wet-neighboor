29643cc651072a4f0dd30d6e6465f6dd0edbb50c
Hermance DREANO <hermance.dreano1@gmail.com>
2025-01-08 11:00:53 +0100

    :sparkles:🍱 windows are closing

--- scripts/windowDisappear.gd
+++ scripts/windowDisappear.gd
@@ -1,15 +1,21 @@
 extends Sprite2D
 
+@export var initial_texture: Texture2D
+@export var clicked_texture: Texture2D
 
+var is_clicked = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	texture = initial_texture
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT :
-		queue_free() # la fenêtre disparait
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT  and event.pressed:
+		if not is_clicked:
+			texture = clicked_texture  # Change la texture au clic
+			is_clicked = true
