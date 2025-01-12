12a192c70e41cdc7b82a0c19575812ff3816b2fe
kalharko <46170581+kalharko@users.noreply.github.com>
2025-01-12 23:05:38 +0100

    :truck: move misplaced script

---
+++ scripts/distance_area.gd
@@ -0,0 +1,14 @@
+extends Area2D
+class_name DistanceArea
+
+# Game design parameters
+@export var additional_height: float = 0
+@export var water_stream_high_point_ratio: float = 0.5
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(_delta: float) -> void:
+	pass
